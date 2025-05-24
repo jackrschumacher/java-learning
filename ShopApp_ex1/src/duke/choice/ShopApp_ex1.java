f276d58860049086c97f85f0904358630267d20d
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2025-05-24 15:10:55 -0500

    Exercise 2

--- ShopApp_ex1/src/duke/choice/ShopApp_ex1.java
+++ ShopApp_ex1/src/duke/choice/ShopApp_ex1.java
@@ -15,6 +15,11 @@ public class ShopApp_ex1 {
      */
     public static void main(String[] args) {
         System.out.println("Welcome to Duke Choice Shop");
+        
+        Customer c1 = new Customer();
+        c1.name = "Pinky";
+
+        System.out.println("Customer is: "+ c1.name); 
     }
     
 }
